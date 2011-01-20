375469c1c8616f3f7b2a246a3dcbc287f1749135
Lifthub Admin <info@lifthub.net>
2011-01-20 16:12:52 +0900

    Added commons.io to the project.

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -18,7 +18,8 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
     "ch.qos.logback" % "logback-classic" % "0.9.26",
     "org.scala-tools.testing" %% "specs" % "1.6.6" % "test->default",
     "org.mockito" % "mockito-core" % "1.8.5" % "test->default",
-    "mysql" % "mysql-connector-java" % "5.1.14"
+    "mysql" % "mysql-connector-java" % "5.1.14", //MySQL
+    "commons-io" % "commons-io" % "2.0.1"
     //"com.h2database" % "h2" % "1.2.138"
   ) ++ super.libraryDependencies
 }
